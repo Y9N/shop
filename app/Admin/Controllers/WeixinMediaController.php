34d81145e354e8b38cfc29795150209a00718adb
root <1522779691@qq.com>
2019-02-21 01:22:04 +0000

    tupian

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -88,7 +88,9 @@ class WeixinMediaController extends Controller
         $grid->media_id('Media id');
         $grid->format('Format');
         $grid->msg_id('Msg id');
-        $grid->local_file_name('Local file name');
+        $grid->local_file_name('Local file name')->display(function($local_file_name){
+            return  '<img src=https://yc.qianqianya.xyz/public/wx/images/'.$local_file_name.'>';
+        });
         $grid->local_file_path('Local file path');
 
         return $grid;
