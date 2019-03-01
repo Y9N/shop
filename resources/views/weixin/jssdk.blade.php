64c31f46724d14f12afb3490219c33148e2e6bf8
root <1522779691@qq.com>
2019-03-01 07:13:09 +0000

    上传图片

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -12,7 +12,6 @@
 <script src="{{URL::asset('/js/jquery-3.2.1.min.js')}}"></script>
 <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 <script>
-    wx.ready(function(){
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: "{{$jssdk['appid']}}", // 必填，公众号的唯一标识
@@ -21,6 +20,7 @@
         signature: "{{$jssdk['sign']}}",// 必填，签名
         jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
     });
+    wx.ready(function(){
     $('#btn').click(function(){
         wx.chooseImage({
             count: 1, // 默认9
