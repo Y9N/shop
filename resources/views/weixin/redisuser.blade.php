3749cb780bf375016132b37a65b6eb43c8be4dad
root <1522779691@qq.com>
2019-03-08 03:00:21 +0000

    复选框

--- resources/views/weixin/redisuser.blade.php
+++ resources/views/weixin/redisuser.blade.php
@@ -1,14 +1,17 @@
+<form action="">
 <table>
     <tr>
+        <td></td>
         <td>openid</td>
-        <td>添加时间</td>
-        <td>用户昵称</td>
-        <td>性别</td>
-        <td>头像</td>
-        <td>关注时间</td>
+        <td style="padding: 5px">添加时间</td>
+        <td style="padding: 5px">用户昵称</td>
+        <td style="padding: 5px">性别</td>
+        <td style="padding: 5px">头像</td>
+        <td style="padding: 5px">关注时间</td>
     </tr>
 @foreach($userinfo as $v)
         <tr>
+            <td><input type="checkbox"></td>
             <td>{{$v['openid']}}</td>
             <td style="padding: 5px">{{$v['add_time']}}</td>
             <td style="padding: 5px">{{$v['nickname']}}</td>
@@ -18,3 +21,4 @@
         </tr>
 @endforeach
 </table>
+</form>
