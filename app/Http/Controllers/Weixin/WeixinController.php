71c04812acfdd0b6eb7d203cdd5452137a98e2ba
root <1522779691@qq.com>
2019-03-08 02:43:39 +0000

    用户信息存到redis

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -203,17 +203,17 @@ class WeixinController extends Controller
             'headimgurl'        => $data['headimgurl'],
             'subscribe_time'    => time(),
         ];
-        $userinfo=Redis::get($this->redis_weixin_user_info);
+        /*$userinfo=Redis::get($this->redis_weixin_user_info);
         if(!empty($userinfo)){
             $info=json_decode($userinfo,true);
             $info[]=$user_data;
             Redis::set($this->redis_weixin_user_info,$info);
             Redis::setTimeout($this->redis_weixin_user_info,3600);
-        }else{
+        }else{*/
             $user_data=json_encode(['userinfo'=>$user_data]);
             Redis::set($this->redis_weixin_user_info,$user_data);
             Redis::setTimeout($this->redis_weixin_user_info,3600);
-        }
+        //}
         return $data;
     }
     /**
