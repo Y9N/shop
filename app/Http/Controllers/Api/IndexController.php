8b308bcf237f87f8f30f2bcbe23784993e3bb812
root <1522779691@qq.com>
2019-03-23 01:29:30 +0000

    sss

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -27,7 +27,7 @@ class IndexController extends Controller
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//
 		//抓取url传给浏览器
 		$rs=curl_exec($ch);
-		return json_encode($rs);
+		return json_decode($rs);
 	}
 
 	public function reg()
